ad371d00bd6c8f7f70d37e6bb66e5e02896bdd4e
Mrunali <mrunaliurade26@gmail.com>
2023-02-25 19:11:19 +0530

    Adding Student file

---
+++ Assign2student.java
@@ -0,0 +1,23 @@
+
+public class Assign2student {
+	String name = "John";
+	int roll_no = 2;
+	
+	void show(){
+		System.out.println(name + " " +            roll_no);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Assign2student s1 = new Assign2student();
+       
+		s1.show();
+	}
+	
+
+}
+
+ 
+ 
+
